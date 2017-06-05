ec0610f385d359ebaa2193c7ed439fed5d354b44
Vincent Scavetta <scavet64@students.rowan.edu>
2017-06-05 23:20:44 +0000

    updating email

--- sendMessage.php
+++ sendMessage.php
@@ -9,10 +9,10 @@ if(isset($_POST["name"]) && isset($_POST["email"]) && isset($_POST["message"])){
     
     $websiteURL = "scavettapps.com";
     $mySiteName = "Scavettapps";
-    $UrlEncodedEmail = urlencode($e);
-    $to = $adminEmail;
+
+    $to = "vstro24@gmail.com";
     $from = $adminEmail;
-    $headers ="From: $from\n";
+    $headers ="From: contact@scavettapps.com\n";
     $headers .= "MIME-Version: 1.0\n";
     $headers .= "Content-type: text/html; charset=iso-8859-1 \n";
     $subject = $mySiteName." Message Received";
