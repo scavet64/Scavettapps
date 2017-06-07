5c80ea8d514f9ef059c8ab23a3e20d2ea2f0f675
Vincent Scavetta <scavet64@students.rowan.edu>
2017-06-07 23:48:37 +0000

    no more index links

--- menu.php
+++ menu.php
@@ -7,7 +7,7 @@
                         <link rel="manifest" href="favicon/manifest.json">
                         <link rel="mask-icon" href="favicon/safari-pinned-tab.svg" color="#5bbad5">
                         <meta name="theme-color" content="#ffffff">
-						<h1><a href="index.php">Scavettapps by Vincent Scavetta</a></h1>
+						<h1><a href="/">Scavettapps by Vincent Scavetta</a></h1>
 						<nav>
 							<a href="#menu">Menu</a>
 						</nav>
@@ -18,7 +18,7 @@
 						<div class="inner">
 							<h2>Menu</h2>
 							<ul class="links">
-								<li><a href="index.php">Home</a></li>
+								<li><a href="/">Home</a></li>
 								<li><a href="https://itunes.apple.com/us/app/supernova-escape/id1212043972?mt=8">SuperNova - Escape!</a></li>
 								<li><a href="https://itunes.apple.com/us/app/incrediball-the-endlessly-rolling-ball/id1119680630?mt=8">Incrediball</a></li>
 								<!--<li><a href="elements.php">Elements</a></li>-->
